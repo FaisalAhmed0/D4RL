46feccc5b8763dda7d476f5d8c398a7a54e34f12
Faisal Ahmed <39379134+FaisalAhmed0@users.noreply.github.com>
2023-07-25 12:08:07 +0000

    removed pybullet

--- setup.py
+++ setup.py
@@ -63,7 +63,6 @@ setup(
         "gym<0.24.0",
         "numpy",
         "mujoco_py",
-        "pybullet",
         "h5py",
         "termcolor",  # adept_envs dependency
         "click",  # adept_envs dependency
